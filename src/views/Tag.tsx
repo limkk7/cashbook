95c4d9d15587d2044a33ac3049313007f2ff937b
linpeng <versionlin@foxmail.com>
2021-10-21 23:07:24 +0800

    封装 findTag 到 useTag

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,6 +1,15 @@
+import { useParams } from "react-router-dom"
+import { useTags } from "./useTags"
+
+type Params = {
+  id: string
+}
 
 const Tag: React.FC = () => {
+  const { tags } = useTags()
+  const { id } = useParams<Params>()
 
-  return (<>hi</>)
+  const tag = findTag(id)
+  return (<>{tag.name}</>)
 }
 export { Tag }
