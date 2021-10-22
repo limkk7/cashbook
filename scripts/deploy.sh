50e93f499404871be2ff9444b50a467ed2ca96ab
linpeng <versionlin@foxmail.com>
2021-10-22 11:06:34 +0000

    添加发布脚本

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bi/env bash
+
+yarn build &&
+cd build
+git init &&
+git add . &&
+git commit -m 'deploy' &&
+git remote add origin git@github.com:versionlin7/cashbook-website.git &&
+git push -u origin master -f
