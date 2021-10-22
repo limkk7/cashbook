3dc576acebebf0fdf351e2f00ca4687ae85a7735
linpeng <versionlin@foxmail.com>
2021-10-22 10:28:12 +0000

    数字键盘输入bug修复

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,5 +1,5 @@
 import Icon from 'components/Icon';
-import React, {useState} from 'react';
+import React from 'react';
 import {generateOuput} from './NumberPadSection/generateOuput';
 import {NumberPadWrapper} from './NumberPadSection/NumberPadWrapper';
 
@@ -9,11 +9,12 @@ interface Obj {
 type Props = {
   value: number;
   onChange: (value: number) => void;
-  onOk?: () => void;
+  onSubmit?: () => void;
+  _setOutput: (x: string) => void;
+  output: string;
 };
 const NumberPadSection: React.FC<Props> = props => {
-  // const output = props.value.toString();
-  const [output, _setOutput] = useState(props.value.toString());
+  // const [output, _setOutput] = useState(props.value.toString());
   const textMap: string[] = ['1', '2', '3', 'delete', '4', '5', '6', 'AC', '7', '8', '9', 'ok', '0', '.'];
   const classMap: Obj = {ok: 'ok', '0': 'zero'};
   const setOutput = (output: string) => {
@@ -23,7 +24,7 @@ const NumberPadSection: React.FC<Props> = props => {
     } else if (output.length === 0) {
       newOutput = '0';
     }
-    _setOutput(newOutput);
+    props._setOutput(newOutput);
     props.onChange(parseFloat(newOutput));
   };
 
@@ -33,16 +34,17 @@ const NumberPadSection: React.FC<Props> = props => {
       return;
     }
     if (value === 'ok') {
-      if (props.onOk) {
-        props.onOk();
+      if (props.onSubmit) {
+        props.onSubmit();
+        // props._setOutput('0');
       }
       return;
     }
-    setOutput(generateOuput(value, output));
+    setOutput(generateOuput(value, props.output));
   };
   return (
     <NumberPadWrapper>
-      <div className="output">{output}</div>
+      <div className="output">{props.output}</div>
       <div className="pad" onClick={onClickButtonWrapper}>
         {textMap.map((text: string) => (
           <button key={text} className={classMap[text] || ''} value={text}>
